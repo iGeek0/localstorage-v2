d20cbf112202610291fa77e135e4989c468ea306
iGeek <jesus.cardenas.d@gmail.com>
2022-08-25 20:41:12 -0500

    Agregado no hay registro

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,32 +19,37 @@ function actualizarStorage() {
 }
 
 function mostrarPeliculas() {
-    tblPeliculas.innerHTML = "";
-    for (const pelicula of peliculas) {
-        const tr = document.createElement("tr");
-        tr.classList.add("text-center");
+    if (peliculas.length === 0) {
+        tblPeliculas.innerHTML = `<tr><td colspan="2" class="text-center font-weight-bold">No hay registros</td></tr>`;
+    } else {
+        tblPeliculas.innerHTML = "";
+        for (const pelicula of peliculas) {
+            const tr = document.createElement("tr");
+            tr.classList.add("text-center");
 
-        const tdPelicula = document.createElement("td");
-        tdPelicula.innerText = pelicula;
-        tr.appendChild(tdPelicula);
+            const tdPelicula = document.createElement("td");
+            tdPelicula.innerText = pelicula;
+            tr.appendChild(tdPelicula);
 
-        const tdAcciones = document.createElement("td");
+            const tdAcciones = document.createElement("td");
 
-        const btnEliminar = document.createElement("button");
-        btnEliminar.classList.add("btn", "btn-danger");
-        btnEliminar.innerText = "Eliminar";
-        btnEliminar.onclick = () => eliminar(pelicula);
-        tdAcciones.appendChild(btnEliminar);
+            const btnEliminar = document.createElement("button");
+            btnEliminar.classList.add("btn", "btn-danger");
+            btnEliminar.innerText = "Eliminar";
+            btnEliminar.onclick = () => eliminar(pelicula);
+            tdAcciones.appendChild(btnEliminar);
 
-        const btnEditar = document.createElement("button");
-        btnEditar.classList.add("btn", "btn-warning", "ml-2");
-        btnEditar.innerText = "Editar";
-        btnEditar.onclick = () => editar(pelicula);
-        tdAcciones.appendChild(btnEditar);
-        tr.appendChild(tdAcciones);
+            const btnEditar = document.createElement("button");
+            btnEditar.classList.add("btn", "btn-warning", "ml-2");
+            btnEditar.innerText = "Editar";
+            btnEditar.onclick = () => editar(pelicula);
+            tdAcciones.appendChild(btnEditar);
+            tr.appendChild(tdAcciones);
 
-        tblPeliculas.appendChild(tr);
+            tblPeliculas.appendChild(tr);
+        }
     }
+
 }
 
 function eliminar(pelicula) {
@@ -59,3 +64,11 @@ function editar(pelicula) {
     peliculas[index] = nuevo_nombre_pelicula;
     actualizarStorage();
 }
+
+
+txtPelicula.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        guardar();
+    }
+});
